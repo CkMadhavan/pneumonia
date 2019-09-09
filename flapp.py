a0c2363c87ddfce1e2397de9e3296fb4e60a9449
Saber Technologies <33689397+CkMadhavan@users.noreply.github.com>
2019-09-09 23:46:16 +0530

    Update flapp.py

--- flapp.py
+++ flapp.py
@@ -68,10 +68,10 @@ def upload_file():
     
       for i in p:
         if i > 0.5:
-            text = "Consider Checking A Doctor (Prediction Percent = " + i[0]*100
+            text = "Consider Checking A Doctor (Prediction Percent = " + str(i[0]*100) + " )"
             x.append(text)
         else:
-            text = "Normal (Prediction Percent = " + 100-(i[0]*100)
+            text = "Normal (Prediction Percent = " + str(100-(i[0]*100))+ " )"
             x.append(text)
 
 
