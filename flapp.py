bc3c92323db5e72a22a61588bdee27bb07434853
Saber Technologies <33689397+CkMadhavan@users.noreply.github.com>
2020-11-14 19:17:13 +0530

    Update flapp.py

--- flapp.py
+++ flapp.py
@@ -148,7 +148,7 @@ def index():
 
 @app.route('/check')
 def check():
-    os.system("wget --load-cookies /tmp/cookies.txt \"https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=14tI0csfkqExAgLdGyiYx1CoYMCX-AYa1' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=14tI0csfkqExAgLdGyiYx1CoYMCX-AYa1\" -O chest.h5 && rm -rf /tmp/cookies.txt")
+    os.system("wget --load-cookies /tmp/cookies.txt \"https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=156c9HyzNm_zLXhM7rWjdATkRBN_oxzFa' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=156c9HyzNm_zLXhM7rWjdATkRBN_oxzFa\" -O chest.h5 && rm -rf /tmp/cookies.txt")
     os.system("wget --load-cookies /tmp/cookies.txt \"https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=14w2OA48YoGdl9hRVdLa8GhNVgS26sjiL' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=14w2OA48YoGdl9hRVdLa8GhNVgS26sjiL\" -O model.json && rm -rf /tmp/cookies.txt")
     
     #with open("model.json") as filename:
