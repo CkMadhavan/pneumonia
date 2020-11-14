d31fd384143c3d7594ca2b2984b237fd0fbc0be9
Saber Technologies <33689397+CkMadhavan@users.noreply.github.com>
2020-11-14 15:36:42 +0530

    Update flapp.py

--- flapp.py
+++ flapp.py
@@ -13,7 +13,7 @@ def index():
 
 @app.route('/check')
 def check():
-    os.system("wget --load-cookies /tmp/cookies.txt \"https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1t2hx34frD16SRsBHehSSGhI-EjAhJ1uv' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1t2hx34frD16SRsBHehSSGhI-EjAhJ1uv\" -O && rm -rf /tmp/cookies.txt")
+    os.system("wget --load-cookies /tmp/cookies.txt \"https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1t2hx34frD16SRsBHehSSGhI-EjAhJ1uv' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1t2hx34frD16SRsBHehSSGhI-EjAhJ1uv\" -O NAME && rm -rf /tmp/cookies.txt")
     return str(os.listdir('.'))
 
 @app.route('/uploader', methods = ['GET', 'POST'])
